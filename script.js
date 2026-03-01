4fe72a3c9addd460f94d9630349a59e0b485c15c
Mayank Sinha <raistar.ff1000.ms@gmail.com>
2026-03-01 19:50:14 +0530

    Toggle active class on button and icon
    Add toggle class to the toggle button for active state.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const navLinks = document.getElementById("nav-links");
 
 toggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
+    toggle.classList.toggle("active");
 
     const icon = toggle.querySelector("i");
     icon.classList.toggle("fa-bars");
