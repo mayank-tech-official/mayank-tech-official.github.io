fda9f58c5213310fde413319ac7a4efd581781f8
Mayank Sinha <raistar.ff1000.ms@gmail.com>
2026-03-01 18:57:20 +0530

    Add menu toggle functionality to script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    const icon = toggle.querySelector("i");
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+});
